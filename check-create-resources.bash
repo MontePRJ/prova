ee9fe4702296f801164730ecc0c5465434ce8c16
MontePRJ <92932214+MontePRJ@users.noreply.github.com>
2021-11-09 17:55:58 +0100

    Update check-create-resources.bash

--- check-create-resources.bash
+++ check-create-resources.bash
@@ -1,20 +1,12 @@
 #!/bin/bash
-
-
 #Utilizziamo questo script per eseguire un check sulle risorse presenti e per creare e aggiornare quelle modificate
-
-
 #Variabili
 export USAGE="Usage ./check-create-resources.bash <PATH_VOLUME> <OCP_PROJECT>"
 export PATH_VOLUME=$1
 export OCP_PROJECT=$2
 export PATH_SETUP_SERVICE="${PATH_VOLUME}/setup.txt"
 export PATH_ELENCO_MICROSERVIZI="${PATH_VOLUME}/elenco_microservizi.csv"
-
-
 export LISTA_MICROSERVIZI=`cat ${PATH_ELENCO_MICROSERVIZI} | grep -v '^#'`
-
-
 for i in `echo $LISTA_MICROSERVIZI`; do
     export NOME_MICROSERVIZIO=`echo $i | cut -d ';' -f 2`
     export BC=`cat ${PATH_SETUP_SERVICE} | grep ^$NOME_MICROSERVIZIO\; | grep ';bc;' | cut -d ';' -f 3`
@@ -22,33 +14,27 @@ for i in `echo $LISTA_MICROSERVIZI`; do
     export SERVICE=`cat ${PATH_SETUP_SERVICE} | grep ^$NOME_MICROSERVIZIO\; | grep ';service;' | cut -d ';' -f 3`
     export ROTTA=`cat ${PATH_SETUP_SERVICE} | grep ^$NOME_MICROSERVIZIO\; | grep ';rotta;' | cut -d ';' -f 3`
     export CONFIGMAP=`cat ${PATH_SETUP_SERVICE} | grep ^$NOME_MICROSERVIZIO\; | grep ';configmap;' | cut -d ';' -f 3`
-    export SECRET=`cat ${PATH_SETUP_SERVICE} | grep ^$NOME_MICROSERVIZIO\; | grep ';secret;' | cut -d ';' -f 3`  
-        
+    export SECRET=`cat ${PATH_SETUP_SERVICE} | grep ^$NOME_MICROSERVIZIO\; | grep ';secret;' | cut -d ';' -f 3`    
     if [[ $BC == "true" ]]; then
       oc delete bc/${NOME_MICROSERVIZIO} -n ${OCP_PROJECT}
       oc create -f ${PATH_VOLUME}/bc.yml -n ${OCP_PROJECT}
     fi
-
     if [[ $DC == "true" ]]; then
       oc delete dc/${NOME_MICROSERVIZIO} -n ${OCP_PROJECT}
       oc create -f ${PATH_VOLUME}/dc.yml -n ${OCP_PROJECT}
     fi         
-
     if [[ $SERVICE == "true" ]]; then
       oc delete service/${NOME_MICROSERVIZIO} -n ${OCP_PROJECT}
       oc create -f ${PATH_VOLUME}/service.yml -n ${OCP_PROJECT}
     fi   
-
     if [[ $ROTTA == "true" ]]; then
       oc delete rotta/${NOME_MICROSERVIZIO} -n ${OCP_PROJECT}
       oc create -f ${PATH_VOLUME}/rotta.yml -n ${OCP_PROJECT}
     fi  
-
     if [[ $CONFIGMAP == "true" ]]; then
       oc delete configmap/${NOME_MICROSERVIZIO} -n ${OCP_PROJECT}
       oc create -f ${PATH_VOLUME}/configmap.yml -n ${OCP_PROJECT}
     fi  
-
     if [[ $SECRET == "true" ]]; then
       oc delete secret/${NOME_MICROSERVIZIO} -n ${OCP_PROJECT}
       oc create -f ${PATH_VOLUME}/secret.yml -n ${OCP_PROJECT}
